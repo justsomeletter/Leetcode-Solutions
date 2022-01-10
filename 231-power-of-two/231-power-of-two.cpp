6834e7a9c5c9063e21a6383ab33c8969497e33a4
justsomeletter <rahulchauhan2060@gmail.com>
2022-01-10 22:57:57 +0530

    Time: 3 ms (35.12%), Space: 6 MB (22.40%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int temp,num=n;
+        bool cond = false;
+        for(int i=0;i<32;i++){
+            if(pow(2,i) == n) {
+                cond = true;
+                break;
+            }
+        }
+        for(int j =-31;j<0;j++){
+        if(pow(2,j) == n){
+            cond = true;
+            break;
+        }
+        }
+        return cond;
+ }
+};
