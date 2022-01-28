97fd41eaa4a5bf1925a535baa1276ad31491fbc1
justsomeletter <rahulchauhan2060@gmail.com>
2022-01-29 01:31:33 +0530

    Time: 38 ms (33.84%), Space: 31.2 MB (18.06%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int pnums[nums.size()];
+        pnums[0] = nums[0];
+        int ans=INT_MAX;
+        bool cond=false;
+        for(int i=1;i<nums.size();i++){ // Making a prefix array
+            pnums[i] = pnums[i-1]+nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            int leftSum = pnums[i]-nums[i];
+            int rightSum = pnums[nums.size()-1]-pnums[i];
+            if(leftSum == rightSum){
+                ans = min(ans,i);
+                cond = true;
+            }
+        }
+        if(cond) return ans;
+        else return -1;
+        
+    }
+};
