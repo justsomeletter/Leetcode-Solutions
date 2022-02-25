8e268365cae978a3b3b583660f8f1eb392422759
justsomeletter <rahulchauhan2060@gmail.com>
2022-02-25 23:14:56 +0530

    Time: 8 ms (55.82%), Space: 11.7 MB (15.13%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -3,36 +3,11 @@ public:
     vector<int> targetIndices(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int> ans;
-        int start=0,end=nums.size()-1;
-        int mPos=100;
-        bool cond=false;
-        while(start<=end){
-            int mid = end + (start-end)/2;
-            if(nums[mid] == target){
-                mPos = min(mid,mPos);
-                end=mid-1;
-                cond=true;
-            }
-            else if(nums[mid]>target)
-                end = mid-1;
-            else start = mid+1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == target)
+            ans.push_back(i);
         }
-        int xPos=mPos;
-        start=0;
-        end=nums.size()-1;
-        while(start<=end){
-            int mid = end + (start-end)/2;
-            if(nums[mid] == target){
-                xPos = max(mid,xPos);
-                start=mid+1;
-            }
-            else if(nums[mid]>target)
-                end = mid-1;
-            else start = mid+1;
-        }
-        if(cond == false) return ans;
-        for(int i=mPos;i<=xPos;i++) ans.push_back(i);
-        //cout<<mPos<<" "<<xPos<<endl;
+        
         return ans;
     }
 };
