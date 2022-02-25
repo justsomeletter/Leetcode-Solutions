db756513b5e5feb19e7cca3c6344cd9f07c3dd24
justsomeletter <rahulchauhan2060@gmail.com>
2022-02-25 22:15:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.48%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int same=0;
-        int jk=jewels.size();
-        int s = stones.size();
-        
-        for(int i=0;i<jk;i++){
-            for(int j=0;j<s;j++){
-                if(jewels[i] == stones[j]) same++;
-            }
+        int freqJ[100]={0};
+        int freqS[100]={0};
+        for(int i=0;i<jewels.size();i++){
+            freqJ[jewels[i]-65]++;
+        }
+        for(int i=0;i<stones.size();i++){
+            freqS[stones[i]-65]++;
+        }
+        for(int i=0;i<=90;i++){
+            if(freqJ[i]!=0) same += freqS[i];
         }
         return same;
     }
