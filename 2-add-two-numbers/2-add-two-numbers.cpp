e5ac2e8910eccc8a92e9f17df150da376ed9c049
justsomeletter <rahulchauhan2060@gmail.com>
2022-03-17 23:06:31 +0530

    Time: 47 ms (59.21%), Space: 72.4 MB (11.77%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,39 +11,14 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //initialisation
-        ListNode *head = NULL,*tail=NULL;
-        int carry=0,num1,num2;
-        while(l1 != NULL or l2 != NULL or carry!=0){
-            if(l1 != NULL){
-                num1 = l1->val;
-                l1=l1->next;
-            }   
-            else 
-                num1=0;
-            if(l2 != NULL){
-                num2 = l2->val; 
-                l2=l2->next;
-            }
-            else 
-                num2 = 0;
-            int sum=num1+num2+carry;
-            if(sum>=10){
-                sum=sum%10;
-                carry=1;
-            }
-            else carry=0;
-            ListNode *temp= new ListNode(sum);
-            if(head == NULL){
-                head=temp;
-                tail=temp;
-            }
-            else{
-                tail->next=temp;
-                tail=temp;
-            }
-            
-        }
-        return head;
+        if(l1 == NULL and l2 == NULL) return NULL;
+        else if(l1 == NULL) return l2;
+        else if(l2 == NULL) return l1;
+        
+        int a = l1->val + l2->val;
+        ListNode *p = new ListNode(a%10);
+        p->next = addTwoNumbers(l1->next,l2->next);
+        if(a>=10) p->next = addTwoNumbers(p->next, new ListNode(1));
+        return p;
     }
 };
