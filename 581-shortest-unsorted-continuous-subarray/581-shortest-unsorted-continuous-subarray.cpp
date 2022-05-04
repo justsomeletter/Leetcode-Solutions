1f9904283fb418308877f9bb58d4a8eff89b3413
justsomeletter <rahulchauhan2060@gmail.com>
2022-05-04 16:09:47 +0530

    Time: 29 ms (88.06%), Space: 26.5 MB (89.70%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> temp(nums.size());
-        for(int i=0;i<nums.size();i++){
-            temp[i] = nums[i];
+        
+        int start=-1,end=-1,size=nums.size();
+        int cur_max = nums[0];
+        int cur_min = nums[size-1];
+        
+        // find the starting index from the starting 
+        for(int i=size-2;i>=0;i--){
+            if(nums[i] > cur_min){
+                //order disrupted here
+                start=i;
+            }
+            cur_min = min(cur_min,nums[i]); // keep track of minimum value in               //arr(i to n)
         }
         
-        int start=-1,end=-1;
-        sort(temp.begin(),temp.end());
-        
-        for(int i=0;i<nums.size();i++){
-            if(temp[i] != nums[i]){
-                if(start == -1){
-                    start=i;
-                }
-                else end=i+1;
+        // find the ending index of the subarray
+        for(int i=1;i<size;i++){
+            if(nums[i]<cur_max){
+                //order is disrupted here 6 < 2
+                end=i+1;
             }
+            cur_max = max(cur_max,nums[i]);
         }
         return end-start;
     }
