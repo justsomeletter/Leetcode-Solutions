ce134bed64b15e7d672ad1ed633ef4e0072e3655
justsomeletter <rahulchauhan2060@gmail.com>
2022-05-18 10:00:49 +0530

    Time: 17 ms (58.53%), Space: 5.9 MB (75.50%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long long ans=0,temp=x;
+        while(temp){
+            ans *= 10;
+            int rem = temp%10;
+            ans += rem;
+            temp =temp/10;
+        }
+        if(ans == x) return true;
+        else return false;
+    }
+};
