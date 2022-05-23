156689a6a2c9349045dfa34df84d6cc45aed809e
justsomeletter <rahulchauhan2060@gmail.com>
2022-05-24 00:13:51 +0530

    Time: 7 ms (82.81%), Space: 14.9 MB (44.03%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -15,42 +15,13 @@ public:
         
         if(list2 == NULL) return list1;
         
-        ListNode *head = NULL,*tail = NULL;
-        
-        while(list1 != NULL and list2 != NULL){
-            
-            //creating new node
-            ListNode *new_node = new ListNode();
-            
-            //to connect the nodes of the linked list
-            if(tail != NULL){
-                tail->next = new_node;
-            }
-            
-            if(list1->val > list2->val){
-                new_node->val = list2->val;
-                list2 = list2->next;
-            }
-            else{
-               new_node->val = list1->val;
-                list1 = list1->next; 
-            }
-            
-            //when first node of final linked list is made
-            if(head == NULL){
-                head = new_node;
-                tail = head;
-            }
-            
-            // to keep track of last node in the newly created linked list
-            tail = new_node;
+        if(list1->val <= list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
         }
-        
-         if(list2 != NULL)
-            tail->next = list2;
-        else if(list1 != NULL)
-            tail->next = list1;   
-        
-        return head;
+        else{
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
+        }
     }
 };
