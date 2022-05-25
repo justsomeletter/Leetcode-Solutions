59196a2270b1d71a3eb660de37a14c14ef2473ac
justsomeletter <rahulchauhan2060@gmail.com>
2022-05-25 16:11:51 +0530

    Time: 69 ms (15.29%), Space: 72.5 MB (12.14%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,8 +11,7 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL and l2 == NULL) return NULL;
-        else if(l1 == NULL) return l2;
+        if(l1 == NULL) return l2;
         else if(l2 == NULL) return l1;
         
         int a = l1->val + l2->val;
