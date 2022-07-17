e4c7f93319d620b3a6ef28cdec9a9ccce13a2365
justsomeletter <rahulchauhan2060@gmail.com>
2022-07-18 00:16:15 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.22%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,24 +3,16 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        sub(0,temp,ans,nums,nums.size());//function call to recursive function
+        int i=0;
+        fun(nums,ans,temp,0);
         return ans;
     }
-    
-    void sub(int ind,vector<int> &temp,vector<vector<int>> &ans, vector<int> &nums,int n){
-        
-        //base case
-        if(ind >= n){
-            ans.push_back(temp);
-            return ;
+    void fun(vector<int> &nums,vector<vector<int>> &ans,vector<int> &temp,int i){
+        ans.push_back(temp);
+        for(int j=i;j<nums.size();++j){
+            temp.push_back(nums[j]);
+            fun(nums,ans,temp,j+1);
+            temp.pop_back();
         }
-        
-        // taking the nums[ind]
-        temp.push_back(nums[ind]);
-        sub(ind+1,temp,ans,nums,n);
-        temp.pop_back();
-            
-        // not taking the nums[ind]
-          sub(ind+1,temp,ans,nums,n);
     }
 };
