7fa5a7bb33ff13030974c6828be365285bdbac1e
justsomeletter <rahulchauhan2060@gmail.com>
2022-07-19 22:27:36 +0530

    Time: 26 ms (35.83%), Space: 13 MB (99.92%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        int r = matrix.size();
-        int c = matrix[0].size();
-        int col0=1;
+        vector<int> dum_row(matrix[0].size(),-1);
+         vector<int> dum_col(matrix.size(),-1);
         
-        for(int i=0;i<r;i++){
-            if(matrix[i][0] == 0) col0=0;
-            for(int j=1;j<c;j++){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0; // set the row matrix indicator to 0
-                    matrix[0][j] = 0; // set the col matrix ind to 0
-                }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                   if(matrix[i][j] == 0){
+                       dum_row[j] = 0;
+                       dum_col[i] = 0;
+                   }
             }
         }
-        
-        // Now print the array int the reverse fashion.
-        for(int i=r-1;i>=0;i--){
-            for(int j=c-1;j>=1;j--){
-                if(matrix[i][0] == 0 or matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
+        //making the column zeroes
+       for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(dum_row[j] == 0) matrix[i][j] = 0;
+                if(dum_col[i] == 0) matrix[i][j] = 0;
             }
-            if(col0 == 0){
-                matrix[i][0] = 0; // set the 0th column to zero
-            }
-        }
+       }
     }
 };
