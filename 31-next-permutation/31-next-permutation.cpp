3cd755608f0f7f91d7ce366f9f9d3c2eb300a3fb
justsomeletter <rahulchauhan2060@gmail.com>
2022-07-20 22:41:31 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (99.82%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -18,6 +18,14 @@ class Solution {
               }
           }
       }
-      reverse(nums.begin()+i+1,nums.end());
+      reverse(nums,i+1,n-1);
   }
+    private:
+    void reverse(vector<int> &nums,int l,int r){
+        while(l<=r){
+            swap(nums[l],nums[r]);
+            l++;
+            r--;
+        }
+    }
 };
